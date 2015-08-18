c45e1fed5e454da5f4f24e1a7412634321bbc4f5
Panagiotis Vakalis <pani.vak@gmail.com>
2015-08-18 14:16:48 +0100

    Model has been finalized

--- src/pxv425/Model.java
+++ src/pxv425/Model.java
@@ -40,31 +40,74 @@ public abstract class Model extends Observable {
 		return client;
 	}
 	
+	/**
+	 * Method to set the client outside of the class
+	 * @param client
+	 * 
+	 * @author Panagiotis Vakalis
+	 * @version 14-07-2015
+	 */
 	public void setClient(Client client){
 		this.client = client;
 	}
 	
-//	public LocalDateTime getCurrentDate(){
-//		return LocalDateTime.now();
-//	}
-	
+	/**
+	 * Method to check whether the stock market is open or closed
+	 * @return true, if the time and the day are ok
+	 * return false, if the time and the day are not ok
+	 * 
+	 * @author Panagiotis Vakalis
+	 * @version 14-07-2015
+	 */
 	private boolean isValidDayAndTime(){
+		/*
+		 * Create a calendar object
+		 */
 		Calendar calendar = Calendar.getInstance();
+		/*
+		 * Get the current day
+		 */
 		int today = calendar.get(Calendar.DAY_OF_WEEK);
 		SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
 		if(today == Calendar.SATURDAY || today == Calendar.SUNDAY){
+			/*
+			 * If the current day is either Saturday or Sunday
+			 * then the stock market is closed.
+			 * So, the method returns false
+			 */
 			return false;
 		}
 		else{
+			/*
+			 * If the current day is neither Saturday nor Sunday
+			 */
 			if(time.format(calendar.getTime()).compareTo("16:29:59") == 1 || time.format(calendar.getTime()).compareTo("09:59:59") == -1){
+				/*
+				 * If the time is after 16:29:59 or before 09:59:59
+				 * then the stock market is closed and the method
+				 * returns false 
+				 */
 				return false;
 			}
 			else{
+				/*
+				 * If the time is between the already mentioned times
+				 * then the marker is open and the method returns
+				 * true
+				 */
 				return true;
 			}
 		}
 	}
 	
+	/**
+	 * Method to check whether the stock market is open or closed
+	 * @return true, if the time and the day are ok
+	 * return false, if the time and the day are not ok
+	 * 
+	 * @author Panagiotis Vakalis
+	 * @version 14-07-2015
+	 */
 	public boolean useIsValidDayAndTime(){
 		return isValidDayAndTime();
 	}
